900cac080e57f44b1e02162e66c0c83e21a5eb7b
Alex Bonine <lemurdev@gmail.com>
2015-03-11 14:29:04 -0500

    Adding config-api skeleton

---
+++ src/config-api.js
@@ -0,0 +1,14 @@
+export default {
+    spotify: {
+        id: '',
+        secret: ''
+    },
+    songkick: {
+        key: ''
+    },
+    echoNest: {
+        apiKey: '',
+        consumerKey: '',
+        sharedSecret: ''
+    }
+};
